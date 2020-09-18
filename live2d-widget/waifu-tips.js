26fced33d8a5c8e7c51e67b4572bfe831d0df3e1
liuyaanng <1757465118@qq.com>
2020-09-18 15:06:45 +0800

    Site updated: 2020-09-18 15:06:44

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -7,7 +7,7 @@ function loadWidget(config) {
 	let { waifuPath, apiPath, cdnPath } = config;
 	let useCDN = false, modelList;
 	if (typeof cdnPath === "string") {
-		useCDN = true;
+		useCDN = false;
 		if (!cdnPath.endsWith("/")) cdnPath += "/";
 	} else if (typeof apiPath === "string") {
 		if (!apiPath.endsWith("/")) apiPath += "/";
@@ -227,14 +227,14 @@ function loadWidget(config) {
 			if (!modelList) await loadModelList();
 			const target = randomSelection(modelList.models[modelId]);
 			loadlive2d("live2d", `${cdnPath}model/${target}/index.json`);
-			showMessage("我的新衣服好看嘛？", 4000, 10);
+			showMessage("主人我的新衣服好看嘛？", 4000, 10);
 		} else {
 			// 可选 "rand"(随机), "switch"(顺序)
 			fetch(`${apiPath}rand_textures/?id=${modelId}-${modelTexturesId}`)
 				.then(response => response.json())
 				.then(result => {
 					if (result.textures.id === 1 && (modelTexturesId === 1 || modelTexturesId === 0)) showMessage("我还没有其他衣服呢！", 4000, 10);
-					else loadModel(modelId, result.textures.id, "我的新衣服好看嘛？");
+					else loadModel(modelId, result.textures.id, "nihao我的新衣服好看嘛？");
 				});
 		}
 	}
