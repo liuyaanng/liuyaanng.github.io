a6f50c5f7e51e7b0f7cf701b75d2a5f91bf73b91
liuyaanng <1757465118@qq.com>
2021-02-25 13:42:23 +0800

    Site updated: 2021-02-25 13:42:22

--- ted/js/mobile.js
+++ ted/js/mobile.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	auto = true;//循环播放模式
 	gi = 0;//初始化GET链接序号
 	url_pre = 'https://cdn.jsdelivr.net/gh/GeekOcean/'
-	url_pre_2 = 'TED_resource@master/txt/'
+	url_pre_2 = 'TED_resource@1.1/txt/'
 	url_name = ['Bilingual','English', 'Raw' ]
 	url_after = '.json'
 	url = url_pre + url_pre_2 + url_name[gi] + url_after;
